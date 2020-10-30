2ba16b9f442992e23b79c89ff7c22cce427e84eb
nicsiv <nicoleusivek@gmail.com>
2020-10-30 10:53:20 -0500

    commit tree 1c0cde44d7cee1fc641307e2bbe858022e9b6871

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name 
+        @@all << self
+        @save = save
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+
+    end
+
+    def self.print_all 
+        puts @@all.map do |dog|
+            dog.name
+        end
+    end
+
+    def save.all
+       @@all << self
+    end
+end
